2861c06e3fe8c1352b6eac204d62d059ab3e6fc5
LFelipeAlvarez <lfalvarez00@gmail.com>
2024-08-27 16:39:47 -0500

    First change

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
 
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
+  console.log('Primer cambio')
   await app.listen(envs.port);
   logger.log(`Gateway running on port ${envs.port}`);
 }
