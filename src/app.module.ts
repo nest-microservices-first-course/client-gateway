512d83674a9d94abaa8004666c2aedcf621f0733
LFelipeAlvarez <lfalvarez00@gmail.com>
2024-09-05 12:00:45 -0500

    health-check endpoint added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { NastModule } from './transports/nast.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, NastModule, AuthModule],
+  imports: [ProductsModule, OrdersModule, NastModule, AuthModule, HealthCheckModule],
 })
 export class AppModule { }
